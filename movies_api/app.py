f5e8967bfd7f64d999adc9382c4c27b15f165b1d
Ayan Cooper <ayancooper01@gmail.com>
2021-02-01 18:48:39 -0500

    Completed slide 14 of API Part 2 for the Movies project

--- movies_api/app.py
+++ movies_api/app.py
@@ -3,6 +3,8 @@ from flask import Flask, render_template, request, json, jsonify, current_app as
 
 app = Flask(__name__)
 
+movies_path = os.path.join(app.static_folder, 'data', 'movies.json')
+
 @app.route('/')
 def index():
     return render_template('index.html')
@@ -10,6 +12,20 @@ def index():
 @app.route('/about')
 def about():
     return '<h1>About</h1><p>Some Other Content</p>'
+
+#Get all movies.
+@app.route('/api/v1/movies/all' methods=['GET'])
+def api_movies_all():
+
+    #using with allows for opening and closing of file
+    with open(movies_path, 'r') as jsondata:
+        movies = json.load(jsondata)
+
+    #no need to return an html template
+    return jsonify(movies)
+
+
+
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
