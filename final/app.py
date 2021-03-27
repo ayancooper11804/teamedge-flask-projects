6110624586709e93a23c3b68f2025519cade08c6
Ayan Cooper <ayancooper01@gmail.com>
2021-03-27 13:02:57 -0400

    Completed steps 1 and 2 for final project

--- final/app.py
+++ final/app.py
@@ -9,5 +9,19 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
+#Get and Post requests
+@app.route('/success/<name>')
+def success(name):
+    return 'welcome %s' & name
+
+@app.route('/login' ,methods = ['POST', 'GET'])
+def login():
+    if request.method == 'POST':
+        user = request.form['nm']
+        return redirect(url_for('success' ,name = user))
+    else:
+        user = request.args.get('nm')
+        return redirect(url_for('success' ,name = user)) 
+
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
