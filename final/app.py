5a3b5331e3f9fb22ce5568917402a017a7643f80
Ayan Cooper <ayancooper01@gmail.com>
2021-03-29 19:55:40 -0400

    Ofiice Hours

--- final/app.py
+++ final/app.py
@@ -1,7 +1,7 @@
 from flask import Flask, render_template, current_app as app, redirect, url_for, request
 from sense_hat import SenseHat
 
-#sense = SenseHat()
+sense = SenseHat()
 
 app = Flask(__name__)
 
@@ -10,18 +10,14 @@ def index():
     return render_template('index.html')
 
 #Get and Post requests
-@app.route('/success/<name>')
-def success(name):
-    return 'welcome %s' & name
+@app.route('/success' ,methods = ['POST'])
+def success():
+    name = request.form['name']
+    messages = request.form['message']
+    sense.show_message(name + message)
+    return render_template('success.html' ,name = name ,message = messages)
 
-@app.route('/login' ,methods = ['POST', 'GET'])
-def login():
-    if request.method == 'POST':
-        user = request.form['nm']
-        return redirect(url_for('success' ,name = user))
-    else:
-        user = request.args.get('nm')
-        return redirect(url_for('success' ,name = user)) 
+
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
