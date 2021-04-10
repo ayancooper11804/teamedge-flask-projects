63334093e234386661c6e29160da04f8a0f5af86
Ayan Cooper <ayancooper01@gmail.com>
2021-04-10 14:05:37 -0400

    Added a database to the final project so all names and messages can be sent.

--- final/app.py
+++ final/app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, current_app as app, redirect, url_for, request
 from sense_emu import SenseHat
+import sqlite3
 
 sense = SenseHat()
 
@@ -15,7 +16,26 @@ def success():
     name = request.form['name']
     messages = request.form['message']
     sense.show_message(name  + " " + messages)
+    conn = sqlite3.connect('./static/data/senseDisplay.db')
+    curs = conn.cursor()
+    curs.execute("INSERT INTO messages (name, nessage) VALUES((?),(?))",(name, messages))
+    conn.commit()
+    conn.close()
     return render_template('success.html' ,name = name ,message = messages)
+
+@app.route('/all')
+def all():
+    conn = sqlite3.connect('./static/data/senseDisplay.db')
+    curs = conn.curso()
+    messages = []
+    rows = curs.execute("SELECT * from messages")
+    for row in rows:
+        message = {'name': row[0], 'message': row[1]}
+        messages.append(message)
+    conn.close()
+    return render_template('all.html', message = messages)
+
+
 
 
 
